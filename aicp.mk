5f91d318d4bd73f9974c3b9d214469702c82158e
Julian Veit <claymore1298@gmail.com>
2018-05-02 22:15:54 +0200

    himawl: AICP modifications

--- aicp.mk
+++ aicp.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, device/htc/himawl/full_himawl.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
-PRODUCT_NAME := lineage_himawl
+PRODUCT_NAME := aicp_himawl
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="htc_himawl" \
@@ -13,3 +13,12 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="4.49.605.11 CL947620 release-keys"
 
 BUILD_FINGERPRINT := "htc/HTCOneM9vzw/htc_himawl:7.0/NRD90M/947620.11:user/release-keys"
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+     DEVICE_MAINTAINERS="Julian Veit (Claymore1297)"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+-include vendor/aicp/config/bootanimation.mk
