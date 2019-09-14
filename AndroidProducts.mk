Claymore1297/android_device_htc_himawl

himawl: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES

AndroidProducts.mk
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_himawl.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_himawl-user \
+    lineage_himawl-userdebug \
+    lineage_himawl-eng

AICP/device_htc_himawl

Change-Id: I1449078734081702aba8f568d4efa9e84d2ffe67

AndroidProducts.mk
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aicp_himawl.mk
+
+COMMON_LUNCH_CHOICES := \
+    aicp_himawl-user \
+    aicp_himawl-userdebug \
+    aicp_himawl-eng